21b47f802a9c5c163a732cd68a4599d4ca048a12
Marc R <marc@mint.fritz.box>
2022-11-01 15:25:30 +0100

    testing

--- scraper.py
+++ scraper.py
@@ -5,7 +5,17 @@ import requests
 
 # https://s.to
 
-URL = 'https://home.speedyland.de/comcave/whatismyip'
-        
-print(URL)
- 
+# URL = 'https://home.speedyland.de/comcave/whatismyip' 
+
+URL = 'https://s.to'                   # Basis URL die Abgerufen werden soll 
+perepherie = '/serie/stream/peripherie' # URL Erweiterung
+andor = '/serie/stream/andor'
+House_of_Dragon = '/serie/stream/house-of-the-dragon'
+Rick_and_Morty ='/serie/stream/rick-and-morty'
+
+sP = (URL + perepherie)
+sA = (URL + andor)
+sHoD = (URL + House_of_Dragon)
+sRaM = ( URL + Rick_and_Morty)
+
+print(sP, sA, sHoD, sRaM) 
