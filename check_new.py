b90aa0db01b2045f4488eb0829f693d49626eb7c
Marc R <marc@fedora.fritz.box>
2022-11-03 18:36:59 +0100

    updates

--- check_new.py
+++ check_new.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://s.to/neue-episoden'
+URL = 'https://s.to/neue-episoden' # Hier wird die URL angegeben
 
 response = requests.get(URL)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-pagestitle = soup.find('span', {'class=': 'col-md-12'})
-print(pagestitle)
+soup = BeautifulSoup(response.text, 'html.parser') # Hier wird BS gesagt, dass es die textdatei als html parsen soll
+
+checknew_data = soup.find('div', {'class', 'newEpisodeList'}) # Hier wird der Filter gesetzt
+checknew_data = checknew_data.text
+print(checknew_data)
