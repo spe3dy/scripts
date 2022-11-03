2137ce0be772215ffabf078895a72a5617e63727
Marc R <marc@fedora.fritz.box>
2022-11-03 19:33:23 +0100

    Updated

--- check_new.py
+++ check_new.py
@@ -5,8 +5,10 @@ URL = 'https://s.to/neue-episoden' # Hier wird die URL angegeben
 
 response = requests.get(URL)
 
-soup = BeautifulSoup(response.text, 'html.parser') # Hier wird BS gesagt, dass es die textdatei als html parsen soll
+soup = BeautifulSoup(response.text, 'html.parser') # Hier wird BeautifullSoup gesagt, dass es die textdatei als html parsen soll
 
-checknew_data = soup.find('div', {'class', 'newEpisodeList'}) # Hier wird der Filter gesetzt
+checknew_data = soup.find('div', {'class', 'newEpisodeList', 'german.svg'} ) # Hier wird der Filter gesetzt
 checknew_data = checknew_data.text
+
+checknew_data = '\n'.join((line for line in checknew_data.split('\n') if line))
 print(checknew_data)
