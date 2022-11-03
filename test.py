a53fede517e1534bee8475f304d3c651a5dc1b23
Spe3dy <spe3dy81@gmail.com>
2022-11-03 15:24:26 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from logging.config import listen
-from bs4 import BeautifulSoup
-import requests
-file = open("quellen.txt", "r")
-for line in file:
-    #print(line)
-    liste.append(line)
-print(liste)
-print("ich habe folgende Ergebnisse gefunden {}") ## Nicht getestet, ging nicht auf dem Windows Rechner
