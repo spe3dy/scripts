a8a813e654b7f94ef1ded6b1f3ea555d7c26f95c
Marc R <marc@fedora.fritz.box>
2022-11-03 17:30:29 +0100

    Updated

--- sonnenaufgang.py
+++ sonnenaufgang.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://sonnenaufgang.online/sun/essen'
+URL = 'https://sonnenaufgang.online/sun/essen_(germany)' # Hier wird die URL angegeben
 
 response = requests.get(URL)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-sunrise_data = soup.find('span', {'data-name': 'sunrise'})
+soup = BeautifulSoup(response.text, 'html.parser') # Hier wird BS gesagt, dass es die textdatei als html parsen soll
+sunrise_data = soup.find('span', {'data-name', 'sunrise'}) # Hier wird der Filter gesetzt
 sunrise_data = sunrise_data.text
-print(sunrise_data)
+#sunrise_data = sunrise_data[:-3] Loescht die letzten 3 Zeichen
+print (sunrise_data)
