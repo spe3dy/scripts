023aa9547e196070e6a2bfc95632e68f1286408a
spe3dy <spe3dy81@gmail.com>
2023-10-17 21:25:02 +0200

    Vergleichsrechner

--- vergleich.py
+++ vergleich.py
@@ -1,16 +1,23 @@
 #vergleiche Arbeiten vs Jobcenter
 
+#Eingang
 M = 325 # Miete
+FG = 42 # Fahrgeld
+
+#Ausgang
 S = 100 # Strom
 G = 88 # Gas
 I = 50 # Internet
 C = 25 # iPad
 F = 50 # Fahrkarte
 H = 10 # Handy
+Sp = 250 # Spesen - Auswaerts Essen
 
-Ausgaben = (M+S+G+I+C+F+H) # Fixkosten
+#Zusammenfassung
+Einnahmen = (M+FG)
+Ausgaben = (M+S+G+I+C+H) # Fixkosten
+
 Jobcenter = 890 # Buergergeld
 ArbeitWCV = 1728.31 #Netto Gehalt WCV
-Spesen = 200 # Auswaerts Essen
 
-print(f"Ausgaben abzueglich Fixkosten ({Ausgaben} Euro): \nWCV {ArbeitWCV - Ausgaben - Spesen} Euro,\nJobcenter {Jobcenter - Ausgaben} Euro.\n Differenz {ArbeitWCV - Jobcenter}")
+print(f"Ausgaben abzueglich Fixkosten ({Ausgaben} Euro): \n WCV {ArbeitWCV - Ausgaben - Sp - F} Euro,\nJobcenter {Jobcenter - Ausgaben + FG -F} Euro.\n Differenz {ArbeitWCV - Jobcenter + FG -F}")
