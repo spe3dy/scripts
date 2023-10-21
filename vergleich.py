f9bc7b19d68c3cce13310c3a6e0a7ba81f2bddc1
spe3dy <spe3dy81@gmail.com>
2023-10-21 13:56:58 +0200

    Updates

--- vergleich.py
+++ vergleich.py
@@ -1,24 +1,24 @@
-#vergleiche Arbeiten vs Jobcenter
+# vergleiche Arbeiten vs Jobcenter
 
-#Eingang
+# Eingang
 M = 325 # Miete
 FG = 42 # Fahrgeld
 
 #Ausgang
-S = 100 # Strom
-G = 88 # Gas
-I = 50 # Internet
-C = 25 # iPad
-F = 50 # Fahrkarte
-H = 10 # Handy
+S = 100  # Strom
+G = 88   # Gas
+I = 50   # Internet
+C = 25   # iPad
+F = 50   # Fahrkarte
+H = 10   # Handy
 Sp = 250 # Spesen - Auswaerts Essen
 
-#Zusammenfassung
+# Zusammenfassung
 Einnahmen = (M+FG)
 Ausgaben = (M+S+G+I+C+H) # Fixkosten
 
 Jobcenter = 890 # Buergergeld
 Fahrgeld = 42 # Fahrgeld vom Jobcenter
-ArbeitWCV = 1728.31 #Netto Gehalt WCV
+ArbeitWCV = 1728.31 # Netto Gehalt WCV
 
 print(f"Ausgaben abzueglich Fixkosten ({Ausgaben} Euro): \n WCV {ArbeitWCV - Ausgaben - Sp - F} Euro,\nJobcenter {Jobcenter - Ausgaben + FG -F} Euro.\n Differenz {ArbeitWCV - Jobcenter + FG -F}")
